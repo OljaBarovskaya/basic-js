fb4f853efbf40a72c973cde3a4e98dece8a4de22
OljaBarovskaya <asdfg1234_08@mail.ru>
2023-04-13 20:26:43 +0300

    g

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function transform(arr){
+  if(!Array.isArray(arr)){throw new Error("'arr' parameter must be an instance of the Array!")}else {
+    let commands = ['--discard-next','--discard-prev', '--double-next', '--double-prev'];
+  let copiedArr=arr.slice();
+  arr.forEach((element,index)=>{
+    if(!commands.includes(element)){} else {
+  copiedArr.splice(-(arr.length - index), 1);
+  if(element==='--discard-next'){
+if(!(arr[index+1]===undefined)){copiedArr.splice(-(arr.length-(index+1)), 1)}
+  }
+  if(element==='--discard-prev'){
+   if(!(arr[index-1]===undefined)) {
+    if(!((arr[index-2]==='--discard-next'))){
+      copiedArr.splice(-(arr.length-(index)), 1);
+    }
+   }
+  }
+  if(element==='--double-next'){
+    if (!(arr[index+1]===undefined)){copiedArr.splice(-(arr.length-(index+1)), 0, arr[index+1])}    
+  }
+  if(element==='--double-prev'){
+if (!(arr[index-1]===undefined)) {
+  if (!((arr[index-2]==='--discard-next'))){
+    copiedArr.splice(-(arr.length-(index)), 0, arr[index-1])
+  }
+}  
+  }
+    }
+  })
+  return copiedArr
+  } 
+  
+  }
 
 module.exports = {
   transform
