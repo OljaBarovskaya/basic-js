e076ede1c569024d92f9ebeb6b67e6083461044c
OljaBarovskaya <asdfg1234_08@mail.ru>
2023-04-14 14:20:11 +0300

    h

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -12,16 +12,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 
-  function deleteDigit(n) {
-    let arr=n.toString().split('');
-    console.log(arr)
+function deleteDigit(n) {
+  let arr=n.toString().split('');
+  console.log(arr)
+  if(arr[0]<arr[1]){arr.splice(0,1)} else {
     let duplicate = arr.slice().sort();
-    console.log(duplicate)
-    let indexOfNumber = arr.indexOf(duplicate[0], 0);
-    arr.splice(indexOfNumber, 1);
-    let number = Number(arr.join(''));
-    return number
-  }
+  console.log(duplicate)
+  let indexOfNumber = arr.indexOf(duplicate[0], 0);
+  arr.splice(indexOfNumber, 1);
+  } 
+  let number = Number(arr.join(''));
+  return number
+}
 
 
 module.exports = {
