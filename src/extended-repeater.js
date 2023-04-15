2b749c96424a816641cce567c1a72e7939d5ecfb
OljaBarovskaya <asdfg1234_08@mail.ru>
2023-04-15 14:42:22 +0300

    g

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,91 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options ) {
+
+  if(str===null){
+    str='null'
+  }
+  if(str===false){
+    str='false'
+  }
+  if(str===true){
+    str='true'
+  }
+  
+  if(!(typeof(str)==='string')){
+    if(Array.isArray(str)){
+      str.join('')
+    } else {
+      str.toString()
+    }
+  }
+
+let repeatTimes=options.repeatTimes;
+let separator='+';
+if(options.separator){
+  separator=options.separator;
+}
+let addition=options.addition;
+
+if(addition===null){
+  addition='null'
+}
+if(addition===false){
+  addition='false'
+}
+if(addition===true){
+  addition='true'
+}
+
+let additionRepeatTimes=options.additionRepeatTimes;
+let additionSeparator='|';
+if(options.additionSeparator){
+  additionSeparator=options.additionSeparator;
+}
+
+
+if(addition){
+if(!(typeof(addition)==='string')){
+  if(Array.isArray(addition)){
+    addition.join('')
+  } else {
+    addition.toString()
+  }
+}
+}
+
+
+ let additionString;
+ if((addition)||(addition===false)){
+  additionString=addition;
+  for (let i=1; i<additionRepeatTimes;i++){
+    additionString=additionString+additionSeparator+addition;
+    }
+ }
+
+let mainString;
+if((addition)||(addition===false)){
+  mainString=str+additionString;
+  for (let i=1; i<repeatTimes;i++){
+    {
+      mainString=mainString+separator+str+additionString;
+    }
+}
+}
+ else {
+    mainString = str;
+    for (let i=1; i<repeatTimes;i++){
+      {
+        mainString=mainString+separator+str;
+      }
+    }
+  }
+
+  
+
+console.log(mainString);
+return mainString;
 }
 
 module.exports = {
--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -25,7 +25,146 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function minesweeper(/* matrix */) {
   throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  const matrix = [
+    [true, false, false],
+    [false, true, false],
+    [false, false, false]
+   ];
+   
+  //  minesweeper(matrix) => [
+  //   [1, 2, 1],
+  //   [2, 1, 1],
+  //   [1, 1, 1]
+  //  ];
+  
+  
+  
+  
+   function minesweeper(matrix) {
+     let matrix2=[];
+     console.log(matrix2)
+      for(let i=0; i<matrix.length; i++){
+        matrix2[i]=[];
+        for(let j=0; j<matrix[i].length; j++){
+          matrix2[i][j]=0;
+        }
+      }
+     console.log(matrix2)
+    for (let i=0; i<matrix.length; i++){
+     for(let j=0; j<matrix[i].length; j++){
+     console.log(matrix2)
+        if(matrix[i][j]===true){
+   //       console.log(`i=${i}, j=${j}`,matrix[i][j])
+          if (i>0 && j>0 && i<(matrix.length-1) && j<(matrix[i].length-1)){
+          if(matrix[i-1][j-1]){matrix2[i-1][j-1]++};
+          if(matrix[i-1][j]){matrix2[i-1][j]++};
+          if(matrix[i-1][j+1]){matrix2[i-1][j+1]++};
+          if(matrix[i][j-1]){matrix2[i][j-1]++};
+          if(matrix[i][j+1]){matrix2[i][j+1]++};
+          if(matrix[i+1][j-1]){matrix2[i+1][j-1]++};
+          if(matrix[i+1][j]){matrix2[i+1][j]++};
+          if(matrix[i+1][j+1]){matrix2[i+1][j+1]++};
+          }
+          if (j>0 && i<(matrix.length-1) && j<(matrix[i].length-1)){
+          if(matrix[i][j-1]){matrix2[i][j-1]++};
+          if(matrix[i][j+1]){matrix2[i][j+1]++};
+          if(matrix[i+1][j-1]){matrix2[i+1][j-1]++};
+          if(matrix[i+1][j]){matrix2[i+1][j]++};
+          if(matrix[i+1][j+1]){matrix2[i+1][j+1]++};
+          }
+          if (i<(matrix.length-1) && j<(matrix[i].length-1)){
+            if(matrix[i][j+1]){matrix2[i][j+1]++};
+            if(matrix[i+1][j]){matrix2[i+1][j]++};
+           if(matrix[i+1][j+1]){matrix2[i+1][j+1]++};
+          }
+          if (j>0 && j<(matrix[i].length-1)){
+            if(matrix[i][j-1]){matrix2[i][j-1]++};
+          if(matrix[i][j+1]){matrix2[i][j+1]++};
+          }
+          if (j>0 && i<(matrix.length-1)){
+            if(matrix[i][j-1]){matrix2[i][j-1]++};
+            if(matrix[i+1][j-1]){matrix2[i+1][j-1]++};
+            if(matrix[i+1][j]){matrix2[i+1][j]++};
+          }
+          if (j<(matrix[i].length-1)){
+            if(matrix[i][j+1]){matrix2[i][j+1]++};
+          }
+          if (j>0){
+            if(matrix[i][j-1]){matrix2[i][j-1]++};
+          }
+          if (i<(matrix.length-1)){
+            if(matrix[i+1][j]){matrix2[i+1][j]++};
+          }
+          if (i>0 && i<(matrix.length-1) && j<(matrix[i].length-1)){
+            if(matrix[i-1][j]){matrix2[i-1][j]++};
+            if(matrix[i-1][j+1]){matrix2[i-1][j+1]++};
+            if(matrix[i][j+1]){matrix2[i][j+1]++};
+            if(matrix[i+1][j]){matrix2[i+1][j]++};
+          if(matrix[i+1][j+1]){matrix2[i+1][j+1]++};
+          }
+          if (i>0 && j<(matrix[i].length-1)){
+            if(matrix[i+1][j]){matrix2[i+1][j]++};
+          if(matrix[i+1][j+1]){matrix2[i+1][j+1]++};
+          }
+          if (i>0 && i<(matrix.length-1)){
+            if(matrix[i-1][j]){matrix2[i-1][j]++};
+            if(matrix[i+1][j]){matrix2[i+1][j]++};
+          }
+          if (i>0){
+            if(matrix[i-1][j]){matrix2[i-1][j]++};
+          }
+          if (i>0 && j>0 &&  j<(matrix[i].length-1)){
+            if(matrix[i-1][j-1]){matrix2[i-1][j-1]++};
+          if(matrix[i-1][j]){matrix2[i-1][j]++};
+          if(matrix[i-1][j+1]){matrix2[i-1][j+1]++};
+         if(matrix[i][j-1]){matrix2[i][j-1]++};
+          if(matrix[i][j+1]){matrix2[i][j+1]++};
+          }
+          if (i>0 && j>0){
+            if(matrix[i-1][j-1]){matrix2[i-1][j-1]++};
+            if(matrix[i-1][j]){matrix2[i-1][j]++};
+            if(matrix[i][j-1]){matrix2[i][j-1]++};
+          }        
+          if (i>0 && j>0 && i<(matrix.length-1)){
+            if(matrix[i-1][j-1]){matrix2[i-1][j-1]++};
+            if(matrix[i-1][j]){matrix2[i-1][j]++};
+            if(matrix[i][j-1]){matrix2[i][j-1]++};
+            if(matrix[i+1][j-1]){matrix2[i+1][j-1]++};
+            if(matrix[i+1][j]){matrix2[i+1][j]++};
+          }
+        }
+      }
+    }
+    return matrix2
+  }
+  
+  
+  
+  console.log(minesweeper(matrix))
+  
+  // [i-1][j-1] [i-1][j] [i-1][j+1]
+  // [i][j-1]    [i][j]   [i][j+1]
+  // [i+1][j-1] [i+1][j] [i+1][j+1]
+  
+  
+  
+  // if (i<(matrix.length-1) && j<(matrix[i].length-1)){
+  //   if(matrix[i][j+1]){matrix2[i][j+1]++};
+  //   if(matrix[i+1][j]){matrix2[i+1][j]++};
+  //           if(matrix[i+1][j+1]){matrix2[i+1][j+1]++};
+  
+  // function minesweeper(matrix) {
+  //     let matrix2=[];
+  //     for (let i=0; i<matrix.length; i++){
+  //        matrix2[i]=[];
+  //       for(let j=0; j<matrix[i].length; j++){
+  //   matrix2[i][j]=0;
+  //   console.log(matrix2)
+  //       }
+  //     }
+  //   }
+  
+  //   console.log(minesweeper(matrix))
 }
 
 module.exports = {
